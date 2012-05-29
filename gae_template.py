f35f9edde720e8ae1a1326c387c80537bbb4ee79
Max Timkovich <mtimkovich@gmail.com>
2012-05-29 00:09:24 -0500

    Initial commit

---
+++ gae_template.py
@@ -0,0 +1,35 @@
+import os
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        self.write("wiki")
+
+PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
+app = webapp2.WSGIApplication(
+    [
+#         ('/signup', Signup),
+#         ('/login', Login),
+#         ('/logout', Logout),
+        ('/_edit' + PAGE_RE, EditPage),
+        (PAGE_RE, WikiPage),
+    ],
+    debug=True)
