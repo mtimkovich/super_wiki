6f8b496988633d2e266450f9c9e6c55fa61c073e
Max Timkovich <mtimkovich@gmail.com>
2012-06-03 14:36:40 -0500

    Added User to models

--- models.py
+++ models.py
@@ -4,3 +4,8 @@ class Article(db.Model):
     path = db.StringProperty(required = True)
     content = db.TextProperty(default = '')
     created = db.DateTimeProperty(auto_now_add = True)
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pass_hash = db.StringProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
