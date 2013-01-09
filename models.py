31f6503dc93126ec54a73725931b0df818779ba3
Max Timkovich <mtimkovich@gmail.com>
2013-01-08 23:08:44 -0600

    Password hashes are now salted

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import hashlib
+import string
+import random
 
 from google.appengine.ext import db
 
@@ -6,6 +8,20 @@ class Article(db.Model):
     path = db.StringProperty(required = True)
     content = db.TextProperty(default = '')
     created = db.DateTimeProperty(auto_now_add = True)
+
+def make_salt(length = 5):
+    return ''.join(random.choice(string.letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return "{},{}".format(salt, h)
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
 
 class User(db.Model):
     username = db.StringProperty(required = True)
@@ -22,7 +38,7 @@ class User(db.Model):
 
     @classmethod
     def register(cls, username, password):
-        pw_hash = hashlib.sha256(password).hexdigest()
+        pw_hash = make_pw_hash(username, password)
 
         return User(username = username,
                     pw_hash = pw_hash)
@@ -30,5 +46,5 @@ class User(db.Model):
     @classmethod
     def login(cls, username, password):
         u = cls.by_name(username)
-        if u and hashlib.sha256(password).hexdigest() == u.pw_hash:
+        if u and valid_pw(username, password, u.pw_hash):
             return u
